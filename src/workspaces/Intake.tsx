afe5daaeb7f2249236aa5d072eb25256cad3f745
piotr <piotr.lewandowski09@gmail.com>
2026-01-20 22:24:28 +0100

    Add calendar component and integrate into Intake view

--- src/workspaces/Intake.tsx
+++ src/workspaces/Intake.tsx
@@ -1,8 +1,33 @@
-function Intake() {
-  return (
-      <div className="intake">
+import {LastDaysCalendar} from "../calories/Calendar.tsx";
+import {useState} from "react";
 
-      </div>)
+function Intake() {
+    // async function getIntake() {
+    //
+    // }
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+
+    const handleDateChange = (date: Date) => {
+        setSelectedDate(date);
+
+        console.log("Wybrano datę w Intake:", date);
+    };
+
+    return (
+        <div className="intake">
+            <div className="intake-calendar-wrapper">
+                <LastDaysCalendar
+                    selectedDate={selectedDate}
+                    onDateSelect={handleDateChange}
+                />
+            </div>
+
+            <div className="intake-content">
+                <p>Widok dla dnia: {selectedDate.toLocaleDateString('pl-PL')}</p>
+
+            </div>
+        </div>
+    );
 }
 
 export default Intake;
